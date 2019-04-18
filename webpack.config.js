91d78ef6e920d678022a776dbb6d6b5c0954ebaa
joshualholland <joshualholland@gmail.com>
2019-04-17 22:59:59 -0500

    shot in the dark

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,73 @@
-var path = require('path');
+const path = require('path');
+var nodeExternals = require('webpack-node-externals');
 
-const CLIENT_DEST = path.join(__dirname, './client/dist');
+const serverConfig = (env, argv) => {
 
-module.exports = {    
-    entry: ['@babel/polyfill', './client/src/index.js'],
-    output: { path: CLIENT_DEST, filename: 'bundle.js' },
-    mode: 'production',
+  return {
+    mode: process.env.NODE_ENV || 'development',
+    entry: './server/src',
     module: {
-        rules: [
-            {
-                test: /.jsx?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react'],
-                    },
+      rules: [
+        {
+            test: /.jsx?$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react'],
                 },
             },
-            {
-                test: /.css?$/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
+        },
+        {
+            test: /.css?$/,
+            use: ['style-loader', 'css-loader']
+        }
+      ]
     },
     resolve: {
-        extensions: ['.js', '.jsx', '.css'],
-    }
+      extensions: ['.jsx', '.js']
+    },
+    output: {
+      filename: 'index.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    target: 'node',
+    node: {
+      __dirname: false
+    },
+    externals: [nodeExternals()]
+  }
 };
+
+const clientConfig = {
+    mode: process.env.NODE_ENV || 'development',
+    entry: './client/src/index.js',
+    devtool: 'inline-source-map',
+    module: {
+      rules: [
+        {
+            test: /.jsx?$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react'],
+                },
+            },
+        },
+        {
+            test: /.css?$/,
+            use: ['style-loader', 'css-loader']
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.jsx', '.js', '.css']
+    },
+    output: {
+      filename: 'app.js',
+      path: path.resolve(__dirname, 'public/js')
+    }
+  };
+
+module.exports = [serverConfig, clientConfig];
